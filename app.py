7cbab4de61976a178bc74432ddf7033b3ac2a2b2
stasvaaas <105922844+stasvaaas@users.noreply.github.com>
2023-04-05 13:44:26 +0300

    working registration and login page

--- app.py
+++ app.py
@@ -1,43 +1,45 @@
 import os
 import psycopg2
-from flask import render_template, request, url_for, redirect, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_manager, current_user, login_required, logout_user, login_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import render_template, request, url_for, redirect, flash, Flask, session
 
-from init_db import db, app
-from models import Movie, User
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:new_password@localhost/movies_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-# app = Flask(__name__)
-# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:new_password@localhost/movies_db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(500), nullable=False)
+    email = db.Column(db.String(150), unique=True)
+
+    def __repr__(self):
+        return f"<your id : {self.user_id}>"
 
 
-# class User(db.Model):
-#     __tablename__ = 'users'
-#     # user_id = db.Column(db.Integer, db.Sequence('user_id_seq'))
-#     username = db.Column(db.String(100), unique=True, primary_key=True)
-#     password = db.Column(db.String(500), nullable=True)
-#     email = db.Column(db.String(150), unique=True)
-#
-#     def __repr__(self):
-#         return f"<your id : {self.user_id}>"
-#
-#
-# class Movie(db.Model):
-#     __tablename__ = 'movies'
-#     name = db.Column(db.String(150))
-#     genre = db.Column(db.String(150))
-#     director = db.Column(db.String(100))
-#     released = db.Column(db.Date)
-#     synopsis = db.Column(db.Text)
-#     rating = db.Column(db.Float)
-#     poster = db.Column(db.LargeBinary)
-#     posted_by = db.Column(db.String(100), db.ForeignKey('users.username'), nullable=False)
-#     movie_id = db.Column(db.Integer, db.Sequence('movie_id_seq'), primary_key=True)
-#
-#     def __repr__(self):
-#         return f"<Movie id: {self.movie_id}>"
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    name = db.Column(db.String(150), nullable=False)
+    genre = db.Column(db.String(150), nullable=False)
+    director = db.Column(db.String(100), nullable=True)
+    released = db.Column(db.Date, nullable=False)
+    synopsis = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    poster = db.Column(db.LargeBinary, nullable=True)
+    posted_by = db.Column(db.String(100), db.ForeignKey('users.username'), nullable=False)
+    movie_id = db.Column(db.Integer, nullable=False, primary_key=True)
+
+    def __repr__(self):
+        return f"<Movie id: {self.id}>"
 
 
 def get_db_connection():
@@ -46,6 +48,11 @@ def get_db_connection():
                             user=os.environ['DB_USERNAME'],
                             password=os.environ['DB_PASSWORD'])
     return conn
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 @app.route("/")
@@ -77,42 +84,58 @@ def movies_all():
 def signup():
     error = None
     if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        phash = generate_password_hash(request.form['password'])
+        # check if password == confirm password
+        if request.form['password'] == request.form['password2']:
+            username = request.form['username']
+            email = request.form['email']
+            phash = generate_password_hash(request.form['password'])
+            max_id = db.session.query(db.func.max(User.id)).scalar()
+            new_id = (max_id or 0) + 1
+            # Validate inputs
+            if not username or not email or not phash:
+                error = 'Please fill out all fields.'
+                flash(error)
+                return render_template('signup.html')
 
-        # Validate inputs
-        if not username or not email or not phash:
-            error = 'Please fill out all fields.'
-            flash(error)
-            return render_template('signup.html')
+            # Check if username or email already exists
+            if User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first():
+                error = 'Username or email already exists.'
+                flash(error)
+                return render_template('signup.html')
 
-        # Check if username or email already exists
-        if User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first():
-            error = 'Username or email already exists.'
-            flash(error)
-            return render_template('signup.html')
-
-        # Add user to database
-        user = User(username=username, email=email, password=phash)
-        db.session.add(user)
-        db.session.commit()
-
-        return redirect(url_for('login'))
+            # Add user to database
+            user = User(id=new_id, username=username, email=email, password=phash)
+            db.session.add(user)
+            db.session.commit()
+            flash('You have successfully registered!', category='success')
+            return redirect(url_for('login'))
 
     return render_template('signup.html', error=error)
 
 
-@app.route('/login', methods=('POST', 'GET'))
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        remember = True if request.form['remember-me'] else False
+        remember = True if request.form.get('remember-me') else False
         user = User.query.filter_by(email=email).first()
         if not user or not check_password_hash(user.password, password):
-            flash('Please, check your login details and try again')
+            flash('Please check your login details and try again.', category='error')
             return redirect(url_for('login'))
-        session['user_id'] = user.user_id
-        return redirect(url_for('movies_all'))
+        login_user(user, remember=remember)
+        return redirect(url_for('home'))
     return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
+    app.run()
