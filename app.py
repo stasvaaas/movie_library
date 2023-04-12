89a13c3b78c6cf0f648dc4b007af193810a3b819
stasvaaas <105922844+stasvaaas@users.noreply.github.com>
2023-04-12 23:24:56 +0300

    upload form

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import os
 import psycopg2
+from flask import Markup
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_manager, current_user, login_required, logout_user, login_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import render_template, request, url_for, redirect, flash, Flask, session
+from werkzeug.utils import secure_filename
 
+
+UPLOAD_FOLDER = 'static/uploads'
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:new_password@localhost/movies_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg'}
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -28,13 +35,13 @@ class User(UserMixin, db.Model):
 
 class Movie(db.Model):
     __tablename__ = 'movies'
-    name = db.Column(db.String(150), nullable=False)
+    title = db.Column(db.String(150), nullable=False)
     genre = db.Column(db.String(150), nullable=False)
     director = db.Column(db.String(100), nullable=True)
     released = db.Column(db.Date, nullable=False)
     synopsis = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Float, nullable=False)
-    poster = db.Column(db.LargeBinary, nullable=True)
+    poster = db.Column(db.String(255), nullable=False)
     posted_by = db.Column(db.String(100), db.ForeignKey('users.username'), nullable=False)
     movie_id = db.Column(db.Integer, nullable=False, primary_key=True)
 
@@ -66,19 +73,46 @@ def movies_all():
     return render_template('movies.html', movies=movies)
 
 
-# @app.route('/create', methods=('GET', 'POST'))
-# def create():
-#     if request.method == 'POST':
-#         title = request.form['title']
-#         genre = request.form['genre']
-#         director = request.form['director']
-#         released = request.form['released']
-#         synopsis = request.form['synopsis']
-#         rating = request.form['rating']
-#         poster = request.form['poster']
-#         #posted_by =
-#
-#     return render_template('create.html')
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if current_user.is_authenticated:
+        if request.method == 'POST':
+            title = request.form['title']
+            genre = request.form['genre']
+            director = request.form['director']
+            released = request.form['released']
+            synopsis = request.form.get('synopsis')
+            rating = request.form['rating']
+            posted_by = current_user.username
+            if 'poster' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['poster']
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                upload_folder = app.config['UPLOAD_FOLDER']
+                if not os.path.exists(upload_folder):
+                    os.makedirs(upload_folder)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                movie = Movie(title=title, genre=genre, director=director, released=released,
+                              synopsis=synopsis, rating=rating, poster=filename, posted_by=posted_by)
+                db.session.add(movie)
+                db.session.commit()
+                return redirect(url_for('home', filename=filename))
+            else:
+                flash('Invalid poster file type. Please upload .png or .jpeg')
+    else:
+        flash(Markup('Please, login to add a new movie.Click <a href="/login">here</a>'))
+        # return redirect(url_for('login'))
+    return render_template('create.html')
+
 
 @app.route('/signup', methods=('POST', 'GET'))
 def signup():
@@ -116,7 +150,7 @@ def signup():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('home'))
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
@@ -127,6 +161,7 @@ def login():
             return redirect(url_for('login'))
         login_user(user, remember=remember)
         return redirect(url_for('home'))
+        #return redirect(request.form.get('next'))
     return render_template('login.html')
 
 
