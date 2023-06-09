3576a61be201da86c8d0f39415ee48b24c6af4f5
stasvaaas <105922844+stasvaaas@users.noreply.github.com>
2023-06-09 12:35:04 +0300

    logging and tests

--- config.py
+++ config.py
@@ -1,11 +1,19 @@
 import os
-
-from flask import Flask, logging
+import logging
+from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 UPLOAD_FOLDER = 'static/uploads'
 app = Flask(__name__)
+
+log_file = 'app.log'
+log_handler = logging.FileHandler(log_file)
+log_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log_handler.setFormatter(formatter)
+
+app.logger.addHandler(log_handler)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:new_password@localhost/movies_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -16,12 +24,4 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
-image_names = os.listdir('static/uploads')
-# logging.basicConfig(level=logging.DEBUG)
-#
-# file_handler = logging.FileHandler('app.log')
-# file_handler.setLevel(logging.DEBUG)
-# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# file_handler.setFormatter(formatter)
-#
-# app.logger.addHandler(file_handler)
+
